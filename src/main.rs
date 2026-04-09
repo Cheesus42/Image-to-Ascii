63fb1e10148ec749a41fb0e8243d7b8a845d2937
Cheesus42 <stumpfleon@proton.me>
2026-04-09 21:50:16 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use std::fs::File;
+use std::io::BufReader;
+
+use ansi_term;
+use png;
+use termsize;
+
+//const BRIGHTNESS: &[u8; 70] = b"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+const BRIGHTNESS: &[u8] = b" .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
+fn to_ascii(f: File) {
+    let mut reader = png::Decoder::new(BufReader::new(f)).read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let info = reader.next_frame(&mut buf).unwrap();
+    let bytes = &buf[..info.buffer_size()];
+    
+    let pixels: Vec<[u8; 4]> = match info.color_type {
+        png::ColorType::Rgb => bytes.chunks_exact(3).map(|byte| [byte[0], byte[1], byte[2], 255]).collect(),
+        png::ColorType::Rgba => bytes.chunks_exact(4).map(|byte| [byte[0], byte[1], byte[2], byte[3]]).collect(),
+        png::ColorType::Grayscale => bytes.iter().map(|&byte| [byte, byte, byte, 255]).collect(),
+        png::ColorType::GrayscaleAlpha => bytes.chunks_exact(2).map(|byte| [byte[0], byte[0], byte[0], byte[1]]).collect(),
+        png::ColorType::Indexed => panic!("indexed color-type is not supported")
+    };
+    
+    //get terminal width
+    let term_size = termsize::get().unwrap_or(termsize::Size { rows: 0, cols: 50 });
+    //determine ratio
+    let ratio = (info.width as f32 / info.height as f32) * 2.0;
+    let h_pixels_per_char = (info.width as f32 / term_size.cols as f32).floor() as usize;
+    let v_pixels_per_char = (h_pixels_per_char as f32 * ratio ).floor().max(1.0) as u32;
+    let rows = info.height as usize / v_pixels_per_char as usize;
+    for dy in 0..rows {
+        for dx in 0..term_size.cols as usize {
+            let mut sum: [u32; 4] = [0; 4];
+            let mut count = 0;
+
+            for y in 0..v_pixels_per_char as usize {
+                for x in 0..h_pixels_per_char as usize {
+                    let px = (dx * h_pixels_per_char as usize + x).min(info.width as usize - 1);
+                    let py = (dy * v_pixels_per_char as usize + y).min(info.height as usize - 1);
+                    let pixel = pixels[py * info.width as usize + px];
+                    sum[0] += pixel[0] as u32;
+                    sum[1] += pixel[1] as u32;
+                    sum[2] += pixel[2] as u32;
+                    sum[3] += pixel[3] as u32;
+                    count += 1;
+                }
+            }
+            if count > 0 {
+                sum[0] /= count;
+                sum[1] /= count;
+                sum[2] /= count;
+                sum[3] /= count;
+            }
+            let color = ansi_term::Colour::RGB(sum[0] as u8, sum[1] as u8, sum[2] as u8);
+            let brightnes_idx = (sum[3] as usize * (BRIGHTNESS.len() - 1)) / 255;
+            let brightness = BRIGHTNESS[brightnes_idx] as char;
+            print!("{}", color.paint(brightness.to_string()));
+        }
+        println!();
+    }
+
+println!(
+        "width: {}, height: {}, color-type: {:?}, bit-depth: {:?}",
+        info.width, info.height, info.color_type, info.bit_depth
+    );
+}
+
+
+fn main() {
+    let file = File::open("test4.png");
+    to_ascii(file.unwrap());
+    println!("Red: {}", ansi_term::Colour::Red.paint("REDDDD"));
+}
